7f750089997ed25d48b577c39b11f4f5bab3c5d5
AlvesF5 <matheus.cruz_@hotmail.com>
2024-02-26 20:48:14 -0300

    Ajustando layout

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -5,6 +5,7 @@ import StepTwo from "@/components/signup/StepTwo"
 import StepThree from "@/components/signup/StepThree"
 import logo from "../../images/logo.png"
 import Image from "next/image"
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
 
 //Hooks
 import userForm from "@/hooks/useForm"
@@ -19,12 +20,17 @@ export default function SignUp() {
 
     return (
         <div>
-            <div className="inputs-container flex flex-col justify-center h-full">
-                <div className=" flex justify-center">
-                    <Image src={logo} width={100} alt="" className="" />
+            <div className="flex flex-col h-full items-center w-full mt-44 gap-8">
+                <div className=" flex">
+                    <Image src={logo} width={240} alt="" className="" />
                 </div>
-                <div>
+               
+                <div className=" w-9/12 md:w-4/12">
                     {currentComponent}
+                </div>
+                <div className='actions flex gap-2 mt-3 text-white font-semibold text-md w-9/12 md:w-4/12 justify-end'>
+                    <button type="button" className='py-3 px-3 bg-balada_violet_500 flex items-center rounded-md uppercase'><GrFormPrevious /><span>Voltar</span></button>
+                    <button type="submit" className='py-1 px-2 bg-balada_green_900 flex items-center rounded-md uppercase'><span>Avançar</span><GrFormNext /></button>
                 </div>
             </div>
         </div>
--- src/components/signup/StepOne.tsx
+++ src/components/signup/StepOne.tsx
@@ -1,19 +1,17 @@
-import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
-
 export default function StepOne() {
     return (
         <div>
-            <main className=" flex w-4/12 mx-auto justify-center items-center h-screen">
+            <main className=" flex w-full mx-auto justify-center items-center h-full">
                 <div className='w-full h-full flex flex-col justify-center'>
                     <div className='flex flex-col gap-8'>
-                        <div className='flex flex-col'>
-                            <h1 className='text-primary-marine-blue text-3xl font-black text-balada_violet_500'>Informações de login</h1>
-                            <p className='text-neutro-cool-gray text-sm text-white'>
-                                Por favor, insira um e-mail e senha válidos
-                            </p>
-                        </div>
                         <form>
                             <div className='flex flex-col gap-4 text-white'>
+                                <div className='flex flex-col justify-start w-full'>
+                                    <h1 className='text-primary-marine-blue text-2xl md:text-3xl font-black text-balada_violet_500'>Informações de login</h1>
+                                    <p className='text-neutro-cool-gray text-sm text-white'>
+                                        Por favor, insira um e-mail e senha válidos
+                                    </p>
+                                </div>
                                 <div className='flex flex-col gap-1'>
                                     <label htmlFor='name' className='text-primary-marine-blue font-medium text-sm'>
                                         Email
@@ -55,10 +53,7 @@ export default function StepOne() {
                                     />
                                 </div>
                             </div>
-                            <div className='actions flex gap-2 mt-3 text-white font-semibold text-md'>
-                                <button type="button" className='py-3 px-3 bg-balada_violet_500 flex items-center rounded-md uppercase'><GrFormPrevious/><span>Voltar</span></button>
-                                <button type="submit" className='py-1 px-2 bg-balada_green_800 flex items-center rounded-md uppercase'><span>Avançar</span><GrFormNext/></button>
-                            </div>
+
                         </form>
                     </div>
 
